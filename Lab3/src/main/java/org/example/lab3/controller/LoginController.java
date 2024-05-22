89d2c600b45eda1f2d2e8c29a45b1e06b201f1eb
phh235 <hoangaatrox23504@gmail.com>
2024-05-22 18:51:58 +0700

    Lab 3 & 4

---
+++ Lab3/src/main/java/org/example/lab3/controller/LoginController.java
@@ -0,0 +1,1 @@
+package org.example.lab3.controller;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestMapping;@Controller@RequestMapping("/account/")public class LoginController {    @GetMapping("/login")    public String ViewLoginForm() {        return "login";    }}
